bf6fd99b1dee6a8a28f1c30d0884dae5894c32bb
sbangar07 <sbangar07@gmail.com>
2022-09-21 23:31:54 +0530

    End of section New

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import React, { useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const {activityStore} = useStore();
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
-      setActivities(response.data);
-    })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+    <>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard />
+      </Container>
 
-        <List>
-        {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default observer(App);
